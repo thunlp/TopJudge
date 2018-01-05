8522a9e96ab0bddaec8bc6c71234b1e6359e1200
zhonghaoxi <zhonghaoxi@yeah.net>
2018-01-06 01:01:01 +0800

    new world

--- net/solver.py
+++ net/solver.py
@@ -2,38 +2,45 @@
 
 import os
 import json
+import configparser
+import torch
+import numpy as np
 
-in_path = "/data/disk1/private/zhonghaoxi/law/data"
+configFilePath = "../config/multi_lstm_crti_baseline_small.config"
+config = configparser.RawConfigParser()
+config.read(configFilePath)
+
+in_path = "/data/disk1/private/zhonghaoxi/law/small_data"
 out_path = "/data/disk1/private/zhonghaoxi/law/format_data"
 
 num_process = 4
 num_file = 20
 
-from data_formatter import parse_sentence
+from data_formatter import parse, check
 
 
 def draw_out(in_path, out_path):
     print(in_path)
     inf = open(in_path, "r")
-    ouf = open(out_path, "w")
 
     cnt = 0
     cx = 0
+    res = []
     for line in inf:
         try:
             data = json.loads(line)
-            res = parse_sentence(data["content"], None)
-            if not (data is None):
-                data["content"] = res
-                print(json.dumps(data), file=ouf)
+            if check(data):
+                res.append(parse(data))
                 cnt += 1
                 if cnt % 50000 == 0:
                     print(in_path, cnt, cx)
                     # break
 
         except Exception as e:
-            #pass  # print(e)
+            # pass  # print(e)
             gg
+
+    np.save(out_path, torch.stack(res).numpy())
 
 
 def work(from_id, to_id):
