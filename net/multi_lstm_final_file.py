7f793c26dc4c4db1de8a8ecf914d51e7cd9d7e51
zhonghaoxi <zhonghaoxi@yeah.net>
2018-01-08 23:47:16 +0800

    add something

---
+++ net/multi_lstm_final_file.py
@@ -0,0 +1,41 @@
+import configparser
+import argparse
+import os
+import pdb
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', '-c')
+parser.add_argument('--gpu', '-g')
+args = parser.parse_args()
+
+configFilePath = args.config
+if configFilePath is None:
+    print("python *.py\t--config/-c\tconfigfile")
+usegpu = True
+# if args.use is None:
+#    print("python *.py\t--use/-u\tcpu/gpu")
+if args.gpu is None:
+    usegpu = False
+else:
+    usegpu = True
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
+
+config = configparser.RawConfigParser()
+config.read(configFilePath)
+
+import torch
+
+from model import MULTI_LSTM_FINAL, train_file
+from file_reader import init_dataset
+
+train_dataset, test_dataset = init_dataset(config)
+
+print("Building net...")
+
+net = MULTI_LSTM_FINAL(config, usegpu)
+if torch.cuda.is_available() and usegpu:
+    net = net.cuda()
+
+print("Net building done.")
+
+train_file(net, train_dataset, test_dataset, usegpu, config)
