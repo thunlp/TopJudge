a0071e592472a2c84a44f39b5f9275b8799c1151
zhonghaoxi <zhonghaoxi@yeah.net>
2018-01-23 22:40:55 +0800

    delete warning

--- net/model.py
+++ net/model.py
@@ -18,7 +18,9 @@ class Attention(nn.Module):
 
     def forward(self, feature, hidden):
         feature = feature.view(feature.size(0), -1, 1)
-        ratio = F.softmax(torch.bmm(hidden, feature))
+        ratio = torch.bmm(hidden,feature)
+        ratio = ratio.view(ratio.size(0),ratio.size(1))
+        ratio = F.softmax(ratio,dim=1)
         result = torch.bmm(hidden.transpose(1, 2), ratio)
         result = result.view(result.size(0), -1)
 
