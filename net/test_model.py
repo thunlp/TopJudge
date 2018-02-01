18f3ded1aff31941d35713be9e6a02ac83d7e929
zhonghaoxi <zhonghaoxi@yeah.net>
2018-02-01 10:49:59 +0800

    testing

---
+++ net/test_model.py
@@ -0,0 +1,58 @@
+import argparse
+import configparser
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', '-m')
+parser.add_argument('--gpu', '-g')
+parser.add_argument('--config', '-c')
+args = parser.parse_args()
+
+configFilePath = args.config
+if configFilePath is None:
+    print("python *.py\t--config/-c\tconfigfile")
+usegpu = True
+# if args.use is None:
+#    print("python *.py\t--use/-u\tcpu/gpu")
+if args.gpu is None:
+    usegpu = False
+else:
+    usegpu = True
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
+
+config = configparser.RawConfigParser()
+config.read(configFilePath)
+
+import torch
+from model import *
+from file_reader import init_dataset
+
+train_dataset, test_dataset = init_dataset(config)
+
+
+print("Building net...")
+net = None
+
+model_name = config.get("net", "name")
+
+if model_name == "CNN":
+    net = CNN(config, usegpu)
+elif model_name == "MULTI_LSTM":
+    net = MULTI_LSTM(config, usegpu)
+elif model_name == "CNN_FINAL":
+    net = CNN_FINAL(config, usegpu)
+elif model_name == "MULTI_LSTM_FINAL":
+    net = MULTI_LSTM_FINAL(config, usegpu)
+else:
+    gg
+if torch.cuda.is_available() and usegpu:
+    net = net.cuda()
+print("Net building done.")
+
+print("Loading model...")
+net.load_state_dict(torch.load(args.model))
+print("Model loaded.")
+
+print("Testing model...")
+test_file(net,test_dataset,usegpu,config,0)
+print("Test done.")
