d3596bbd089faac3d0a0d9b0244779d328a022fb
zhonghaoxi <zhonghaoxi@yeah.net>
2018-04-16 10:42:22 +0800

    gg

--- net/result/gen_result.py
+++ net/result/gen_result.py
@@ -1,8 +1,5 @@
 import json
 
-accusation_file = open("../../data_processor/accusation_list2.txt","r")
-accusation_list = json.loads(accusation_file.readline())
-
 f = open("count_data/total.txt","r")
 nowb = ""
 
@@ -10,7 +7,7 @@ for line in f:
 	line = line[:-1]
 	if line == "law":
 		nowb = "law"
-		ouf = open("law_result1.txt","w")
+		ouf = open("law_result.txt","w")
 	elif line == "crit":
 		nowb = "crit"
 		ouf = open("crit_result.txt","w")
@@ -20,13 +17,16 @@ for line in f:
 		if line == "":
 			continue
 		if nowb == "crit":
-			data = line.split(" ")
-			a = int(data[0])
+			data = line[:-1].split(" ")
+            a = data[0]
 			b = int(data[1])
-			print(accusation_list[a],a,b,file=ouf)
-		else:
-			data = line.split(" ")
-			c = int(data[1])
-			b = int(int(data[0])%10)
-			a = int(int(data[0])//10)
-			print(a,b,c,file=ouf)
+			print(a,b,file=ouf)
+		elif nowb == "law":
+			data = line[:-1].replace("(","").replace(",","").replace(")","").split(" ")
+            a = int(data[0])
+            b = int(data[1])
+            c = int(data[2])
+            d = int(data[3])
+			print(a,b,c,d,file=ouf)
+        else:
+            pass
