814224422ffbf09a10fd1f2c8827a852d317c147
zhonghaoxi <zhonghaoxi@yeah.net>
2018-05-12 14:43:19 +0800

    back

--- net/model/encoder/cnn_encoder.py
+++ net/model/encoder/cnn_encoder.py
@@ -21,11 +21,9 @@ class CNNEncoder(nn.Module):
         gram = config.getint("net", "min_gram")
         for conv in self.convs:
             # print("gg",type(x))
-            y = F.max_pool1d(F.relu(conv(x)).view(config.getint("data", "batch_size"), config.getint("net", "filters")),
+            y = F.max_pool1d(F.relu(conv(x)).view(config.getint("data", "batch_size"), config.getint("net", "filters"),-1),
                              kernel_size=config.getint("data", "sentence_num") * config.getint("data",
-                                                                                               "sentence_len") - gram + 1)
-            print(y)
-            gg
+                                                                                               "sentence_len") - gram + 1).view(config.getint("data","batch_size"),-1)
             # y = F.pad(y,
             #          (0, config.getint("data", "sentence_num") * config.getint("data", "sentence_len") - len(y[0][0])))
             conv_out.append(y)
@@ -35,7 +33,7 @@ class CNNEncoder(nn.Module):
         # conv_out = conv_out.view(config.getint("data", "batch_size"), -1,
         #                         config.getint("data", "sentence_num") * config.getint("data", "sentence_len"))
 
-        self.attention = conv_out.transpose(1, 2)
+        #self.attention = conv_out.transpose(1, 2)
         # print(conv_out)
         fc_input = torch.max(conv_out, dim=2)[0]
         # print(fc_input)
